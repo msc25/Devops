67625f4511ab238ca9bfdbcec5d4d79e4c266d41
Meghrajsinh Chauhan <meghrajsinhchauhan@meghrajsinhsmbp.home>
2024-09-07 11:34:26 -0400

    modify code to call app1 from app2

--- Flask Microservice/app2/app2.py
+++ Flask Microservice/app2/app2.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask,jsonify
+import requests
+
 app = Flask(__name__)
 @app.route("/")
-def helloworld():
-    return "Hello World app2!"
+def hello():
+    try:
+        response = requests.get('http://localhost:8008/hello')
+        data = response.json()
+        return jsonify(message="Hello from app2!", app1_message=data['message'])
+    except requests.exceptions.RequestException as e:
+        return jsonify(message="Failed to connect to app1", error=str(e))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8005, debug=True)
