0872816930d6f7eff724a575ccac64a0e3d81233
Meghrajsinh Chauhan <meghrajsinhchauhan@Meghrajsinhs-MacBook-Pro.local>
2024-09-21 15:07:19 -0400

    updated AWS details and removed hardcoding

---
+++ terraform/05-create-EC2-instance/data-provider.tf
@@ -0,0 +1,23 @@
+data "aws_subnets" "default_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_default_vpc.default.id]
+  }
+}
+
+data "aws_ami" "aws-linux-2023-latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami_ids" "aws-linux-2023-latest_ids" {
+  owners = ["amazon"]
+}
